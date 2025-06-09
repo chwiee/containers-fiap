2c14e6c46f66cbbc6da7be70bf50bd483daee75f
[!] BrunoO <38006634+chwiee@users.noreply.github.com>
2025-06-09 13:14:02 +0000

    add - app resources

--- app/deploy/terraform.tfvars
+++ app/deploy/terraform.tfvars
@@ -1,7 +1,3 @@
-cluster_name = "app-prod"
-
-vpc_id = "vpc-0dcee16646817da54"
-
 subnets_id = [
   "subnet-08c8c8cca48bafef5",
   "subnet-0f9e916c3771c910c",
--- infra/provider.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket   = "wb-aws-tf-state"
-        key      = "wb=tf=ecs.tfstate"
-        region   = "us-east-1"
-    }
-}
